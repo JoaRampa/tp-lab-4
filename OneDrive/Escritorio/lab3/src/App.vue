2e13382d14a71315341b7768264e3a5753294df4
Joaquin Rampa <rampajoa@gmail.com>
2024-03-08 12:52:40 -0300

    routing

--- OneDrive/Escritorio/lab3/src/App.vue
+++ OneDrive/Escritorio/lab3/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Log In</router-link> |
+    <router-link to="/about">Criptos</router-link>
   </nav>
   <router-view />
 </template>
