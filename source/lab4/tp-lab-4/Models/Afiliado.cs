d893089c4d3957031959b4c33304f84b2e6f15f6
Joaquin Rampa <rampajoa@gmail.com>
2024-08-09 14:43:56 -0300

    fix

---
+++ source/lab4/tp-lab-4/Models/Afiliado.cs
@@ -0,0 +1,12 @@
+﻿namespace tp_lab_4.Models
+{
+    public class Afiliado
+    {
+        public int Id { get; set; }
+        public string Apellidos { get; set; }
+        public string Nombres { get; set; }
+        public int DNI { get; set; }
+        public DateTime fechaNacimiento { get; set; }
+        public string? foto { get; set; }
+    }
+}
---
+++ source/lab4/tp-lab-4/Models/Ticket.cs
@@ -0,0 +1,11 @@
+﻿namespace tp_lab_4.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public int afiliadoId { get; set; }
+        public Afiliado afiliados { get; set; }
+        public DateTime fechaSolicitud { get; set; }
+        public bool descripcion { get; set; }
+    }
+}
